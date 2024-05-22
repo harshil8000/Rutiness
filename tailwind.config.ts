7945bbe40168459a4e2f3579e393be2266bba4bb
Harshil Barad <90130954+harshil8000@users.noreply.github.com>
2024-05-22 18:31:48 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,64 @@
+import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+
+  
+  theme: {
+
+    extend: {
+      colors:{
+        'back':'#FFCCDD',
+        'bul':'#CBE9F9',
+        'bluee':'#2E85B9',
+        'bgb':'#E0E6F2',
+    },
+    animation: {
+      aurora: "aurora 60s linear infinite",
+    },
+    boxShadow: {
+      input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+    },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          
+      },
+    },
+    keyframes: {
+      aurora: {
+        from: {
+          backgroundPosition: "50% 50%, 50% 50%",
+        },
+        to: {
+          backgroundPosition: "350% 50%, 350% 50%",
+        },
+      },
+    },
+  },
+  plugins: [
+    addVariablesForColors,
+  ],
+  
+};
+export default config;
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
